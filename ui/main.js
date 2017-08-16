2fc4472fed84d6ee3ad6a6b55690268dd53759f5
jiffingomez <30854022+jiffingomez@users.noreply.github.com>
2017-08-16 19:08:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var text = document.getElementById('text');
-text.innerHTML = 'Helloooooo';
+var element = document.getElementById('text');
+element.innerHTML = 'Helloooooo';
