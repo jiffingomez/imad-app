217ef86288d78cccda19dff1fb1cd228594049d1
jiffingomez <30854022+jiffingomez@users.noreply.github.com>
2017-08-16 19:26:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById('text');
 element.innerHTML = 'Helloooooo';
 var img = document.getElementById('myimage');
-img.onClick = function(){
+img.onclick = function(){
     //var interval = setInterval(moveLeft, 100);
     img.style.marginleft = '500px';
 
