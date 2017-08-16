3e238dfbb8984472a547cf4db05d4ef41409f392
jiffingomez <30854022+jiffingomez@users.noreply.github.com>
2017-08-16 23:16:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,9 @@
-console.log('Loaded!');
-var element = document.getElementById('text');
-element.innerHTML = 'Helloooooo';
-var img = document.getElementById('myimage');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-  var interval = setInterval(moveRight, 50);
+var btn = document.getElementById('counter');
+var counter = 0;
+btn.onclick = function(){
+
+counter = counter + 1;
+var span = document.getElementById('count');
+span.innerHTML = counter.toString();
 };
+
