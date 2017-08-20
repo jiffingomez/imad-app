4c63042e9556c1145d891a2c2246416553f4f4a3
jiffingomez <30854022+jiffingomez@users.noreply.github.com>
2017-08-20 19:05:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,17 @@
 var btn = document.getElementById('counter');
-var counter = 0;
-btn.onclick = function(){
 
-counter = counter + 1;
-var span = document.getElementById('count');
-span.innerHTML = counter.toString();
+btn.onclick = function(){
+var request = new XMLHttpRequest();
+request.onreadystatechange = function(){
+  if(request.readyState === XMLHttpRequest.DONE){
+      if(request.status === 200){
+          var counter = response.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+      }
+  }  
+};
+request.open('GET', 'http://jiffingomez.imad.hasura-app.io/counter', true);
+request.send(null);
 };
 
