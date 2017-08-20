747d988468ad7835f54e5e317950f7114d915ffd
jiffingomez <30854022+jiffingomez@users.noreply.github.com>
2017-08-20 19:10:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var request = new XMLHttpRequest();
 request.onreadystatechange = function(){
   if(request.readyState === XMLHttpRequest.DONE){
       if(request.status === 200){
-          var counter = response.responseText;
+          var counter = request.responseText;
           var span = document.getElementById('count');
           span.innerHTML = counter.toString();
       }
