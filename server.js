192b09834b92e3c976c6603132d5505f0328885f
jiffingomez <30854022+jiffingomez@users.noreply.github.com>
2017-08-30 07:21:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
+
+var config = {
+    user: 'jiffingomez',
+    database: 'jiffingomez',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 
 var app = express();
@@ -61,6 +70,20 @@ function createTemplate(data){
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res){
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    });
+    
 });
 
 var counter = 0;
